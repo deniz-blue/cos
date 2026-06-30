e024d06b72ea20ad162e83816c7b246b2ef36884
deniz <43997085+deniz-blue@users.noreply.github.com>
2026-06-30 13:46:23 +0300

    ci fix 6 -- pin gradle 8.x

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,6 +8,9 @@ pnpm install --frozen-lockfile
 
 npx expo prebuild --platform android --clean --no-install
 
+# Pin Gradle to 8.x (IBM_SEMERU removed from 9.x, breaks plugins)
+sed -i 's|gradle-9\.[0-9]*\.[0-9]*-bin\.zip|gradle-8.14.4-bin.zip|' /workspace/android/gradle/wrapper/gradle-wrapper.properties
+
 # Generate local.properties for the SDK inside the container
 echo "sdk.dir=$ANDROID_HOME" > /workspace/android/local.properties
 
